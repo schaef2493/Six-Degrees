1aa1481dadeab9872b7cab9b9a0c620f8cc9b76d
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-01-30 13:18:16 -0500

    Basic control UI

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+  $('#up').click(function() {
+  	console.log("Translate up");
+  });
+
+  $('#down').click(function() {
+  	console.log("Translate down");
+  });
+
+  $('#left').click(function() {
+  	console.log("Translate left");
+  });
+
+  $('#right').click(function() {
+  	console.log("Translate right");
+  });
+
+});
