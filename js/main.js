eb6f65206bcc44e80e16f9fd9429ed000cc09bec
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-02-11 12:52:02 -0500

    Updated to new ROS library

--- js/main.js
+++ js/main.js
@@ -1,31 +1,53 @@
+// State tracking
 var up_pressed = false;
 var down_pressed = false;
 var left_pressed = false;
 var right_pressed = false;
 
-var connection = new ros.Connection("ws://hostname:9090");
 
-connection.setOnClose(function (e) {
-  console.log('connection closed');
+// Connecting to ROS
+var ros = new ROSLIB.Ros();
+
+// If there is an error on the backend, an 'error' emit will be emitted.
+ros.on('error', function(error) {
+  console.log(error);
 });
 
-connection.setOnError(function (e) {
-  console.log('error!');
+// Find out exactly when we made a connection.
+ros.on('connection', function() {
+  console.log('Connection made!');
 });
 
-connection.setOnOpen(function (e) {
-  console.log('connected to ROS');
+// Create a connection to the rosbridge WebSocket server.
+ros.connect('ws://localhost:9090');
+
+// Configure the twist topic
+var cmdVel = new ROSLIB.Topic({
+  ros : ros,
+  name : '/cmd_vel',
+  messageType : 'geometry_msgs/Twist'
 });
 
 function print_instruction(pressed,dir,v) {
-  movement_vector = '';
+  var message = null;
 
   // TODO: This allows us to send an empty movement vector
   // TODO: Figure out how these vectors map to the arm movement
   // (-0.5, 0.5) is the range of input
 
   if (dir == 'up') {
-      movement_vector = '{"linear":{"x":0.5,"y":0.0,"z":0}, "angular":{"x":0.0,"y":0.0,"z":0.0}}';
+      message = new ROSLIB.Message({
+        linear : {
+          x : 0.5,
+          y : 0.0,
+          z : 0.0
+        },
+        angular : {
+          x : 0.0,
+          y : 0.0,
+          z : 0.0
+        }
+      });
   } 
   else if (dir == 'down') {
 
@@ -39,35 +61,45 @@ function print_instruction(pressed,dir,v) {
 
   if (pressed) {
     console.log("stop " + dir);
-    movement_vector = '{"linear":{"x":0.0,"y":0.0,"z":0}, "angular":{"x":0.0,"y":0.0,"z":0.0}}';
+    message = new ROSLIB.Message({
+        linear : {
+          x : 0.0,
+          y : 0.0,
+          z : 0.0
+        },
+        angular : {
+          x : 0.0,
+          y : 0.0,
+          z : 0.0
+        }
+      });
   }
   else {
     console.log("start " + dir + " " + v);
   }
 
-  console.log('movement: ' + movement_vector);
-  connection.publish('/cmd_vel', 'geometry_msgs/Twist', movement_vector);
+  cmdVel.publish(message);
 }
 
 $(document).ready(function() {
 
   $('#up').click(function() {
-  	print_instruction(up_pressed,"up",1);
+  	print_instruction(up_pressed,"up",0.5);
     up_pressed = !up_pressed;
   });
 
   $('#down').click(function() {
-  	print_instruction(down_pressed,"down",1);
+  	print_instruction(down_pressed,"down",0.5);
     down_pressed = !down_pressed;
   });
 
   $('#left').click(function() {
-  	print_instruction(left_pressed,"left",1);
+  	print_instruction(left_pressed,"left",0.5);
     left_pressed = !left_pressed;
   });
 
   $('#right').click(function() {
-  	print_instruction(right_pressed,"right",1);
+  	print_instruction(right_pressed,"right",0.5);
     right_pressed = !right_pressed;
   });
 
