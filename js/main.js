489fcb52267fba6067e0e0a3f235f70228c3690e
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-02-11 23:06:18 -0500

    ROS Bug Fixes
    via Tekin. This should work with the arm now.

--- js/main.js
+++ js/main.js
@@ -24,55 +24,47 @@ ros.connect('ws://localhost:9090');
 // Configure the twist topic
 var cmdVel = new ROSLIB.Topic({
   ros : ros,
-  name : '/cmd_vel',
+  name : '/twist',
   messageType : 'geometry_msgs/Twist'
 });
 
-function print_instruction(pressed,dir,v) {
-  var message = null;
+var message = new ROSLIB.Message({
+  linear : {
+    x : 0.0,
+    y : 0.0,
+    z : 0.5
+  },
+  angular : {
+    x : 0.0,
+    y : 0.0,
+    z : 0.0
+  }
+});
 
+function print_instruction(pressed,dir,v) {
   // TODO: This allows us to send an empty movement vector
   // TODO: Figure out how these vectors map to the arm movement
   // (-0.5, 0.5) is the range of input
 
   if (dir == 'up') {
-      message = new ROSLIB.Message({
-        linear : {
-          x : 0.5,
-          y : 0.0,
-          z : 0.0
-        },
-        angular : {
-          x : 0.0,
-          y : 0.0,
-          z : 0.0
-        }
-      });
+      message.linear.z = -0.5;
   } 
   else if (dir == 'down') {
-
+      message.linear.z = 0.5;
   } 
   else if (dir == 'left') {
-
+      message.linear.x = 0.5;
   }
   else if (dir == 'right') {
-
+      message.linear.x = -0.5;
   }
 
   if (pressed) {
     console.log("stop " + dir);
-    message = new ROSLIB.Message({
-        linear : {
-          x : 0.0,
-          y : 0.0,
-          z : 0.0
-        },
-        angular : {
-          x : 0.0,
-          y : 0.0,
-          z : 0.0
-        }
-      });
+    
+    message.linear.x = 0;
+    message.linear.y = 0;
+    message.linear.z = 0;
   }
   else {
     console.log("start " + dir + " " + v);
@@ -84,22 +76,22 @@ function print_instruction(pressed,dir,v) {
 $(document).ready(function() {
 
   $('#up').click(function() {
-  	print_instruction(up_pressed,"up",0.5);
+    print_instruction(up_pressed,"up",0.5);
     up_pressed = !up_pressed;
   });
 
   $('#down').click(function() {
-  	print_instruction(down_pressed,"down",0.5);
+    print_instruction(down_pressed,"down",0.5);
     down_pressed = !down_pressed;
   });
 
   $('#left').click(function() {
-  	print_instruction(left_pressed,"left",0.5);
+    print_instruction(left_pressed,"left",0.5);
     left_pressed = !left_pressed;
   });
 
   $('#right').click(function() {
-  	print_instruction(right_pressed,"right",0.5);
+    print_instruction(right_pressed,"right",0.5);
     right_pressed = !right_pressed;
   });
 
