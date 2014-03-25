905c92bd7bc60be7274345b1b3538bc370e13cb1
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-03-24 23:13:48 -0400

    Added socket close code

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,5 +1,10 @@
 var socket = io.connect(window.location.hostname);
 
+window.onbeforeunload = function() {
+	socket.onclose = function () {}; // disable onclose handler first
+    socket.close()
+};
+
 socket.on('tasks', function (data) {
   console.log(data);
 });
--- public/javascripts/robot.js
+++ public/javascripts/robot.js
@@ -1,6 +1,11 @@
 // connect socket
 var socket = io.connect(window.location.hostname);
 
+window.onbeforeunload = function() {
+    socket.onclose = function () {}; // disable onclose handler first
+    socket.close()
+};
+
 // connect to ROS
 var ros = new ROSLIB.Ros();
 
